8a6c2032bbac8ad05553edb4e424cac7e3c345b4
mattagoni <46663698+mattagoni@users.noreply.github.com>
2024-09-13 13:38:25 +1000

    Update create_pain.ps1

--- create_pain.ps1
+++ create_pain.ps1
@@ -1,2 +1,8 @@
 ##Deploy PaloFirewall into Hub vNET
 New-AzResourceGroupDeployment -ResourceGroupName "magoni" -TemplateFile "/home/matthew/new-azure/paindeploytemplate.json" -TemplateParameterFile "/home/matthew/new-azure/paindeployparameters.json"
+
+#Add AllowAll to Mgt interface
+$nic = Get-AzNetworkInterface -ResourceGroupName "magoni" -Name "magoni-pan01964"
+$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName "magoni" -Name "AllowAll"
+$nic.NetworkSecurityGroup = $nsg
+$nic | Set-AzNetworkInterface
