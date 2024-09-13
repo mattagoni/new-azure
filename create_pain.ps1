997cfaf3fbdbd74242876d3f540226d45113abe9
mattagoni <46663698+mattagoni@users.noreply.github.com>
2024-09-13 13:56:59 +1000

    Update create_pain.ps1

--- create_pain.ps1
+++ create_pain.ps1
@@ -2,7 +2,7 @@
 New-AzResourceGroupDeployment -ResourceGroupName "magoni" -TemplateFile "/home/matthew/new-azure/paindeploytemplate.json" -TemplateParameterFile "/home/matthew/new-azure/paindeployparameters.json"
 
 #Add AllowAll to Mgt interface
-$nic = Get-AzNetworkInterface -ResourceGroupName "magoni" -Name "magoni-pan01964"
+$nic = Get-AzNetworkInterface -ResourceGroupName "magoni" -Name "pan01-nic"
 $nsg = Get-AzNetworkSecurityGroup -ResourceGroupName "magoni" -Name "AllowAll"
 $nic.NetworkSecurityGroup = $nsg
 $nic | Set-AzNetworkInterface
